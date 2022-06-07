4f94a97572cf4525b88e57440245eabd9b741c5f
zoop1259 <kdm432191@gmail.com>
2022-06-07 17:34:20 +0900

    Initial Commit

---
+++ TimelineViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimelineViewExample
+//
+//  Created by 강대민 on 2022/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimelineViewExample/TimelineViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewExampleApp.swift
+//  TimelineViewExample
+//
+//  Created by 강대민 on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct TimelineViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
